ad3ce8cb64dbe7d29486f50f7d63d73f43572dad
Feng <gokevin0720k@gmail.com>
2025-05-08 00:13:08 +0800

    Done

--- scripts.js
+++ scripts.js
@@ -10,23 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
 
-        // 顯示帳密（純前端模擬用）
         console.log("輸入的帳號:", username);
         console.log("輸入的密碼:", password);
 
-        // 模擬假登入成功
         alert("登入成功！（模擬）\n帳號：" + username + "\n密碼：" + password);
 
-        // 如果你想傳送到伺服器，這邊可以用 fetch，例如：
-        // fetch('https://your-server.com/save', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({ username, password })
-        // }).then(res => res.json())
-        //   .then(data => console.log("伺服器回應：", data))
-        //   .catch(err => console.error("錯誤：", err));
+        const accountData = `帳號: ${username}\n密碼: ${password}\n`;
+
+        const blob = new Blob([accountData], { type: 'text/plain' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'account.txt';
+        link.click();
     });
 });
-
